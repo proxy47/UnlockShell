fe095987954363b2c8a1499229fbe94b5697a0a2
Marius Grecu <iustingrecu@yahoo.com>
2020-02-03 21:26:47 +0200

    USH-006 : Cleanup

--- UnlockShell/Form1.cs
+++ UnlockShell/Form1.cs
@@ -52,38 +52,6 @@ namespace UnlockShell
         }
     }
 
-    public class ListEntry
-    {
-        public  String model_s;
-        public  String manufacturer_s;
-        public  String destination_s;
-        private String exePath_s;
-        public  String partNo_ls;
-
-        public ListEntry()
-        {
-            this.model_s = String.Empty;
-            this.manufacturer_s = String.Empty;
-            this.destination_s = String.Empty;
-            this.exePath_s = String.Empty;
-            this.partNo_ls = null;
-        }
-
-        public ListEntry(String model_s, String manufacturer_s, String destination_s, String exePath_s, String partNo_ls)
-        {
-            this.model_s = model_s;
-            this.manufacturer_s = manufacturer_s;
-            this.destination_s = destination_s;
-            this.exePath_s = exePath_s;
-            this.partNo_ls = partNo_ls;
-        }
-
-        public String getExePath()
-        {
-            return this.exePath_s;
-        }
-    }
-
     public enum My_enum{
         MODEL_NAME        = 0,
         MANUFACTURER_NAME    ,
